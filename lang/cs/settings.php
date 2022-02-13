68ca9c813ed183b4f4650e9011f1c6afe57457b1
David 'ilicz' Klementa <ilicz@inmail.cz>
2022-02-13 17:19:11 +0100

    Add cs localization

---
+++ lang/cs/settings.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Czech language file for oauthgeneric plugin
+ *
+ * @author David 'ilicz' Klementa <ilicz@inmail.cz>
+ */
+
+$lang['key'] = 'Aplikační UID';
+$lang['secret'] = 'Aplikační heslo';
+$lang['authurl'] = 'URL pro autentizaci';
+$lang['tokenurl'] = 'URL pro získání tokenu';
+$lang['userurl'] = 'Relativní URL pro získání uživatelských informací z API (musí vracet JSON data autentizovaného uživatele)';
+$lang['authmethod'] = 'Autorizační metoda pro získání uživatelských informací z API';
+$lang['scopes'] = 'Scopes to request (comma separated)';
+$lang['scopes'] = 'Požadovaná oprávnění (scopes, oddělená čárkou)';
+
+$lang['json-user'] = 'Objektová cesta k uživatelskému jménu (tečková notace)';
+$lang['json-name'] = 'Objektová cesta k celému jménu uživatele (tečkovánotace)';
+$lang['json-mail'] = 'Objektová cesta k emailu uživatele (tečková notace)';
+$lang['json-grps'] = 'Objektová cesta ke skupinám uživatele (tečková notace)';
+
+$lang['label'] = 'Titulek přihlašovacího tlačítka';
+$lang['color'] = 'Barva přihlašovacího tlačítka';
+
+$lang['authmethod_o_0'] = 'Hlavička OAuth';
+$lang['authmethod_o_1'] = 'Hlavička Bearer';
+$lang['authmethod_o_6'] = 'Hlavička Token';
+$lang['authmethod_o_2'] = 'Query String v1';
+$lang['authmethod_o_3'] = 'Query String v2';
+$lang['authmethod_o_4'] = 'Query String v3';
+$lang['authmethod_o_5'] = 'Query String v4';
